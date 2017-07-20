d553fdd93eb1aa5a4b62d497d7a899874f62d2f7
santi <snt.v.01@gmail.com>
2017-07-20 10:31:46 +0200

    soptifySeeker (OOP)

--- js/script.js
+++ js/script.js
@@ -1,117 +1,127 @@
+// santi's spotify seeker (OOP)
 
-	var token= 'BQAE6wM35LYh_OoRzLA5gfsnwSeoSQaNs5oyR9ypTNGCRzhBMfs0rp9-e7rq0B4i_9T4USorH7DXgBXjJ-krm5b9rBltx8agD9lf_iAMXFQZYMpvY_LeemZSirWHFx0noNurLuolgWs';
-	$(document).submit(function(event){event.preventDefault();});
-	$('button').click(function(){	
-			var search=	$('#artistSearch').val();	
-			var url = 'https://api.spotify.com/v1/search?q=+'+search+'&type=artist';
-			searchArtist(url);
-	})
-			
-	function searchArtist(url){
-		$.ajax({
-				url: url,
-				headers: {
-			        Authorization: 'Bearer ' + token
-			     }
-			})
-			.done(function(results){			//recibimos los datos
-				 listingArtists(results.artists.items);
-				 $('.artist-item').click(function(){
-				 	searchAlbum($(this).attr('data-id'));
-				 })
-			})
-			.fail(function(){
-				alert('conexion faild');
-			});	
-	}
-	
+var token = 'BQBr16ylrqmwWWKU3iWtGV5463T_pWh-7xRFD71zFZFVaejr-WvXl7rEdGmXfdiN6qlxyB0C4-LPj5irNq9i19kdXBFJC5rRK492s3tYHY3yr_XTxgPe0k5lB9u2oRH0SSxCqt9ftPI';
+$(document).submit(function(event) { event.preventDefault(); });
 
-	function searchAlbum(artistId){
-		var urlAlbumSearch='https://api.spotify.com/v1/artists/'+artistId+'/albums';
-			$.ajax({
-				url: urlAlbumSearch,
-				headers: {
-			        Authorization: 'Bearer ' + token
-			     }
-			})
-			.done(function(results){			   //recibimos los datos
-				 $('#albumList').empty();
-				 $('#songsList').empty();
-				 listingAlbums(results.items);
-				 $('.album-item').click(function(){
-				 	listingSongs($(this).attr('data-id'));
-				 	imageBackground(($(this).attr('data-image')));
-				 }) 
-			})
-			.fail(function(){
-				alert('conexion faild');
-			});	
-	}		
+$('button').click(function() {
+    var search = $('#artistSearch').val();
+    spotifyService.searchArtist(search, seekerView.listArtists);
+});
 
-	function listingArtists(results){
-			$('#artistsList').empty();
-			$('#albumList').empty();
-			$('#songsList').empty();
-			var repeatedArtist='';
-			results.forEach(function(artist){
-				if(artist.name!==repeatedArtist){
-					repeatedArtist=artist.name;
-					$('#artistsList').append('<li><a class="artist-item" data-id="' + artist.id + '" href="#">'+artist.name+'</a></li>')
-				}else{}
-			})
-	}
+// business logic
 
-	function listingAlbums(results){
-			var repeatedAlbum='';
-			results.forEach(function(album){
-				if(album.name!==repeatedAlbum){
-					repeatedAlbum=album.name;
-					$('#albumList').append('<li><a class="album-item" data-id="' + album.id + '" data-image="'+album.images[0].url+'" href="#">'+album.name+'</a></li>')
-				}else{}
-			})
-	}
-	function listingSongs(song){
-		var songUrl= 'https://api.spotify.com/v1/albums/'+song+'/tracks';
-			$.ajax({
-				url: songUrl,
-				headers: {
-			        Authorization: 'Bearer ' + token
-			     }
-			})
-			.done(function(results){
-				$('#songsList').empty();
-				var repeatedSong='';
-				results.items.map(function(item){
-					if(item.name!==repeatedSong){
-						repeatedSong=item.name;
-						$('#songsList').append('<li><a class="songs-item" data-song="'+item.preview_url+'" href="#">'+item.name+'</a></li>');
-					}else{}
-				})	
-				$('.songs-item').click(function(){
-					reproduce($(this).attr('data-song'), $(this).text());
-				})		   
-			})
-			.fail(function(){
-				alert('conexion faild');
-			});	
-	}
-	function reproduce(urlPreview, title){
-				if(urlPreview == 'null'){
-					$('.modal-title').text(title);
-	                $('.modal-body').text("I'm sorry, this song is unavailable");
-	                $('#modal').modal('show');
-				}
-				else{
-					$('.modal-title').text(title);
-	                $('.modal-body').html('<audio controls><source src="'+urlPreview+'" type="audio/mpeg"></audio>');
-	                $('#modal').modal('show');
-	            }
+var spotifyService = {
+    searchArtist: function(artistQuery, callback) {
+        $.ajax({
+                url: 'https://api.spotify.com/v1/search?q=+' + artistQuery + '&type=artist',
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            })
+            .done(function(results) { //recibimos los datos
+                callback(results.artists.items);
+            })
+            .fail(function() {
+                alert('conexion faild');
+            });
+    },
+    searchAlbums: function(artistId, callback) {
+        $.ajax({
+                url: 'https://api.spotify.com/v1/artists/' + artistId + '/albums',
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            })
+            .done(function(results) { //recibimos los datos
+                callback(results.items);
+            })
+            .fail(function() {
+                alert('conexion faild');
+            });
+    },
+    searchSongs: function(albumId, callback) {
+        $.ajax({
+                url: 'https://api.spotify.com/v1/albums/' + albumId + '/tracks',
+                headers: {
+                    Authorization: 'Bearer ' + token
+                }
+            })
+            .done(function(results) {
+                callback(results.items);
+            })
+            .fail(function() {
+                alert('conexion faild');
+            });
+    }
+};
 
-	}
-	function imageBackground(img){
-			var image= 'url("'+img+'")';
-				$('#modal').css('background-image', image);
-				$('#modal').css('background-repeat', 'no-repeat');
-				$('#modal').css('background-size', 'cover');
+// presentation logic
 
-	}
+var seekerView = {
+    listArtists: function(artists) {
+        $('#artistsList').empty();
+        $('#albumList').empty();
+        $('#songsList').empty();
+        var repeatedArtist = '';
+        artists.forEach(function(artist) {
+            if (artist.name !== repeatedArtist) {
+                repeatedArtist = artist.name;
+                $('#artistsList').append('<li><a class="artist-item" data-id="' + artist.id + '" href="#">' + artist.name + '</a></li>')
+            }
+        });
+        $('.artist-item').click(function() {
+            spotifyService.searchAlbums($(this).attr('data-id'), seekerView.listAlbums);
+        });
+    },
+
+    listAlbums: function(albums) {
+        $('#albumList').empty();
+        $('#songsList').empty();
+        var repeatedAlbum = '';
+        albums.forEach(function(album) {
+            if (album.name.toLowerCase() !== repeatedAlbum) {
+                repeatedAlbum = album.name.toLowerCase();
+                $('#albumList').append('<li><a class="album-item" data-id="' + album.id + '" data-image="' + album.images[0].url + '" href="#">' + album.name + '</a></li>')
+            }
+        });
+        $('.album-item').click(function() {
+            spotifyService.searchSongs($(this).attr('data-id'), seekerView.listSongs);
+            seekerView.imageBackground(($(this).attr('data-image')));
+        });
+    },
+
+    listSongs: function(songs) {
+        $('#songsList').empty();
+        var repeatedSong = '';
+        songs.forEach(function(item) {
+            if (item.name !== repeatedSong) {
+                repeatedSong = item.name;
+                $('#songsList').append('<li><a class="songs-item" data-song="' + item.preview_url + '" href="#">' + item.name + '</a></li>');
+            }
+        })
+        $('.songs-item').click(function() {
+            seekerView.openSongPlayer($(this).attr('data-song'), $(this).text());
+        })
+    },
+
+    openSongPlayer: function(url, title) {
+        if (url == 'null') {
+            $('.modal-title').text(title);
+            $('.modal-body').text("I'm sorry, this song is unavailable");
+            $('#modal').modal('show');
+        } else {
+            $('.modal-title').text(title);
+            $('.modal-body').html('<audio controls><source src="' + url + '" type="audio/mpeg"></audio>');
+            $('#modal').modal('show');
+        }
+
+    },
+
+    imageBackground: function(img) {
+        var image = 'url("' + img + '")';
+        $('#modal').css('background-image', image);
+        $('#modal').css('background-repeat', 'no-repeat');
+        $('#modal').css('background-size', 'cover');
+
+    }
+};
